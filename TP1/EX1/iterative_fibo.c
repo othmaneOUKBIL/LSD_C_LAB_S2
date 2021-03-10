012c336c892ea2b3a2abe115034dc5e35beee6cb
othmaneOUKBIL <80427030+othmaneOUKBIL@users.noreply.github.com>
2021-03-11 00:04:03 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,18 @@
 
 int iterative_fibo(int n)
 {
+   
+	int a=0; int b=1 ; int c=0;
     // write your iterative_fibo code here
+    if (n==0)
+    	return 0;
+    if (n==1)
+    	return 1;
+    if (n>1)
+    {
+        for(int i=1; i<n;i++)
+        { c=a+b;a=b;b=c;}
+    }
+   return c;
+
 }
